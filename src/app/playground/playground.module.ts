39355ad65a36a2ddb96d94e327a053eaba68d40a
Maks Zhitlov <maks.zhitlov@gmail.com>
2017-05-17 17:29:03 +0300

    change structure

--- src/app/playground/playground.module.ts
+++ src/app/playground/playground.module.ts
@@ -2,25 +2,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { ZelectModule } from '../components/zelect/zelect.module';
 import { PlaygroundComponent } from './playground.component';
 import { ZelectPlaygroundComponent } from './zelect/zelect.component';
-import {ValidatorsModule} from "../validator/validator.module";
+import { ValidatorsModule } from '../validator/validator.module';
+import { ZelectModule } from '../components/zelect/zelect.module';
 
 @NgModule({
-    declarations: [
-        PlaygroundComponent,
-        ZelectPlaygroundComponent
-    ],
-    imports: [
-        CommonModule,
-        FormsModule,
-        RouterModule,
-        ReactiveFormsModule,
-        ZelectModule,
-      ValidatorsModule
-    ],
-    providers: []
+  declarations: [
+    PlaygroundComponent,
+    ZelectPlaygroundComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule,
+    ZelectModule,
+    ValidatorsModule
+  ],
+  providers: []
 })
 export class PlaygroundModule {
 
