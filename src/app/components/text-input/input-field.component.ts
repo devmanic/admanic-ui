9aaa72892628830baa906f8d2d019f5bc296bf22
Maks Zhitlov <maks.zhitlov@gmail.com>
2017-05-23 12:54:13 +0300

    input-container

--- src/app/components/text-input/input-field.component.ts
+++ src/app/components/text-input/input-field.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, Directive, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Directive({
   selector: 'input[admanic], textarea[admanic]',
@@ -17,12 +18,36 @@ export class AdmInputDirective {
   selector: 'adm-input-container',
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['styles.scss'],
+  host: {
+    '[class.adm-input__container]': 'true',
+    '[class.is-invalid]': 'invalid',
+    '[class.with-addon]': '!!addonIcon'
+  },
   template: `
     <div>
-      <ng-content></ng-content>
+      <!--<pre>{{invalid}}</pre>-->
+      <div class="wrap">
+        <div *ngIf="!!addonIcon" class="adm-input__addon">
+          <i class="material-icons">{{addonIcon}}</i>
+        </div>
+        <ng-content></ng-content>
+      </div>
+      <div *ngIf="control && invalid">
+        <validator-messages [field]="control"></validator-messages>
+      </div>
     </div>
   `
 })
 export class AdmInputContainer {
+  @Input() control: FormControl;
+  @Input() addonIcon: string = '';
 
+  get invalid(): boolean {
+    if (this.control) {
+      if (this.control.touched) {
+        return this.control.invalid;
+      }
+    }
+    return false;
+  }
 }
--- src/app/components/text-input/input-field.module.ts
+++ src/app/components/text-input/input-field.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AdmInputContainer, AdmInputDirective } from './input-field.component';
 import { CommonModule } from '@angular/common';
+import { ValidatorsModule } from '../../validator/validator.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ValidatorsModule],
   exports: [AdmInputContainer, AdmInputDirective],
   declarations: [AdmInputContainer, AdmInputDirective],
   providers: []
