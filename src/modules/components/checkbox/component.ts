8591fe09fc386685897a834103771cfe79d23bc8
Maks Zhitlov <maksym.zhitlov@gmail.com>
2017-06-03 03:26:15 +0300

    add ability set value, add label, add form control input

--- src/modules/components/checkbox/component.ts
+++ src/modules/components/checkbox/component.ts
@@ -1,5 +1,4 @@
 import {
-    ChangeDetectionStrategy,
     Component,
     ElementRef,
     EventEmitter,
@@ -7,7 +6,7 @@ import {
     Input,
     Output, ViewEncapsulation
 } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 export const CHECKBOX_CONTROL_VALUE_ACCESSOR: any = {
     provide: NG_VALUE_ACCESSOR,
@@ -21,44 +20,54 @@ export const CHECKBOX_CONTROL_VALUE_ACCESSOR: any = {
     encapsulation: ViewEncapsulation.None,
     styleUrls: ['./style.scss'],
     template: `
-        <label *ngIf="label"></label>
-        <div class="adm-checkbox__el"></div>
+        <div class="adm-checkbox__wrap">
+            <div class="adm-checkbox__el" (click)="toggle();"></div>
+            <div class="adm-checkbox__label" [class.is-required]="_required" (click)="toggle();">
+                <ng-content></ng-content>
+            </div>
+        </div>
     `,
     host: {
         'class': 'adm-checkbox',
-        '(click)': 'toggle()',
-        '[attr.checked]': '_checked',
-        '[class.is-checked]': '_checked',
+        '[attr.checked]': 'checked',
+        '[class.is-checked]': 'checked',
         '[attr.disabled]': 'disabled',
-        '[class.is-disabled]': 'disabled'
+        '[class.is-disabled]': 'disabled',
+        '[class.is-rtl]': 'rtl'
     }
 })
 export class CheckboxComponent {
-    id: string = String(Date.now());
-
-    get inputId(): string {
-        return `adm-checkbox-${this.id}`;
-    }
-
-    _checked: boolean;
+    _checked: boolean | string;
     _required: boolean;
+    _ctrl: FormControl;
 
     @Input() disabled: boolean;
-    @Input() required: boolean;
+    @Input() rtl: boolean;
     @Input() value: string;
 
     @Input()
-    set checked(val: boolean) {
-        this._checked = val;
+    set control(ctrl: FormControl) {
+        this._required = ctrl.hasError('required');
+        this._ctrl = ctrl;
+        console.log(this._required);
     }
 
-    @Output() change: EventEmitter<boolean> = new EventEmitter();
+    @Input()
+    set checked(val: boolean | string) {
+        this._checked = !!val;
+    }
+
+    get checked(): boolean | string {
+        return !this.value ? this._checked : this._checked == this.value;
+    }
+
+    @Output() change: EventEmitter<boolean | string> = new EventEmitter();
 
     constructor(private el: ElementRef) {
     }
 
     writeValue(val: any) {
-        this._checked = !!val;
+        this._checked = val;
         this.onChange(this._checked);
         this.onTouched();
         this.change.emit(this._checked);
@@ -66,7 +75,7 @@ export class CheckboxComponent {
 
     toggle(): void {
         if (!this.disabled) {
-            this.writeValue(!this._checked);
+            this.writeValue(!this.value ? !this._checked : this._checked == this.value ? null : this.value);
         }
     }
 
