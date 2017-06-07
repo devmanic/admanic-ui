4c6085306c0fd0f10ff0c658c8ac61bd008b009b
Maks Zhitlov <maks.zhitlov@gmail.com>
2017-06-07 11:31:09 +0300

    checkbox & radio component

---
+++ src/modules/components/checkbox/control.component.ts
@@ -0,0 +1,113 @@
+import {
+    Component,
+    ElementRef,
+    EventEmitter,
+    forwardRef,
+    Input,
+    Output, ViewEncapsulation
+} from '@angular/core';
+import { FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+export const CHECKBOX_CONTROL_VALUE_ACCESSOR: any = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => CheckboxControlComponent),
+    multi: true
+};
+
+const CHECKBOX_CLASS: string = 'adm-checkbox';
+const RADIO_CLASS: string = 'adm-radio';
+
+@Component({
+    providers: [CHECKBOX_CONTROL_VALUE_ACCESSOR],
+    selector: 'adm-checkbox, adm-radio',
+    encapsulation: ViewEncapsulation.None,
+    styleUrls: ['./style.scss'],
+    template: `
+        <div class="{{baseClassName}}__wrap">
+            <div class="{{baseClassName}}__el" (click)="toggle();"></div>
+            <div class="{{baseClassName}}__label" [class.is-required]="_required" (click)="toggle();">
+                <ng-content></ng-content>
+            </div>
+        </div>
+    `,
+    host: {
+        '[class]': 'baseClassName',
+        '[attr.checked]': 'checked',
+        '[class.is-checked]': 'checked',
+        '[attr.disabled]': 'disabled',
+        '[class.is-disabled]': 'disabled',
+        '[class.is-rtl]': 'rtl'
+    }
+})
+export class CheckboxControlComponent {
+    _checked: boolean | string;
+    _required: boolean;
+    _ctrl: FormControl;
+    _type: 'checkbox' | 'radio' = 'checkbox';
+
+    @Input() disabled: boolean;
+    @Input() rtl: boolean;
+    @Input() value: string;
+
+    @Input()
+    set control(ctrl: FormControl) {
+        this._required = ctrl.hasError('required');
+        this._ctrl = ctrl;
+    }
+
+    @Input()
+    set checked(val: boolean | string) {
+        this._checked = !!val;
+    }
+
+    get checked(): boolean | string {
+        if (this.isRadio) {
+            return this._checked;
+        }
+        return !this.value ? this._checked : this._checked == this.value;
+    }
+
+    @Output() change: EventEmitter<boolean | string> = new EventEmitter();
+
+    constructor(private el: ElementRef) {
+        this._type = el.nativeElement.localName === 'adm-checkbox' ? 'checkbox' : 'radio';
+    }
+
+    get isCheckbox(): boolean {
+        return this._type === 'checkbox';
+    }
+
+    get isRadio(): boolean {
+        return this._type === 'radio';
+    }
+
+    get baseClassName(): string {
+        return this.isRadio ? RADIO_CLASS : CHECKBOX_CLASS;
+    }
+
+    writeValue(val: any) {
+        this._checked = val;
+        this.onChange(this._checked);
+        this.onTouched();
+        this.change.emit(this._checked);
+    }
+
+    toggle(): void {
+        if (!this.disabled) {
+            this.writeValue(!this.value ? !this._checked : this._checked == this.value ? null : this.value);
+        }
+    }
+
+    onChange: Function = (_: any) => {
+    };
+    onTouched: Function = () => {
+    };
+
+    registerOnChange(fn: Function): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: Function): void {
+        this.onTouched = fn;
+    }
+}
---
+++ src/modules/components/checkbox/multi-radio.component.ts
@@ -0,0 +1,89 @@
+import { AfterViewInit, Component, forwardRef, Input, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
+
+export interface option {
+    value: string;
+    label: string
+}
+
+@Component({
+    selector: 'adm-multi-radio',
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => MultiRadioComponent),
+        multi: true
+    }],
+    host: {
+        'class': 'adm-multi-radio',
+        '[class.is-inline]': 'inline'
+    },
+    styleUrls:['./multi-selector.style.scss'],
+    template: `
+        <div [formGroup]="_form" *ngIf="_options.length" class="adm-multi-ctrl-wrap">
+            <adm-radio
+                    formControlName="model"
+                    [value]="item.value"
+                    [checked]="item.value == _form.get('model').value"
+                    *ngFor="let item of _options">
+                {{item.label}}
+            </adm-radio>
+        </div>
+    `
+})
+export class MultiRadioComponent implements OnDestroy, AfterViewInit {
+    _options: option[] = [];
+    _value: string;
+    _model: FormControl = new FormControl(null);
+    _form: FormGroup = new FormGroup({
+        model: this._model
+    });
+    _subscribers: Subscription[] = [];
+
+    @Input() inline: boolean;
+
+    @Input()
+    set options(opts: any[]) {
+        if (typeof opts[0] === 'string') {
+            this._options = opts.map((el: string) => ({
+                value: el,
+                label: el
+            }));
+        }
+    }
+
+    ngAfterViewInit() {
+        this._model.setValue(this._value);
+        this._subscribers.push(
+            this._model.valueChanges.filter(val => !!val).subscribe((value) => {
+                this.writeValue(value != this._value ? value : null);
+            })
+        );
+    }
+
+    writeValue(val: any) {
+        this._value = val;
+        this.onChange(this._value);
+        this.onTouched();
+        if (!this._value) {
+            this._form.get('model').setValue(null);
+        }
+    }
+
+    onChange: Function = (_: any) => {
+    };
+    onTouched: Function = () => {
+    };
+
+    registerOnChange(fn: Function): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: Function): void {
+        this.onTouched = fn;
+    }
+
+    ngOnDestroy() {
+        this._subscribers.forEach((el: Subscription) => el.unsubscribe());
+    }
+}
