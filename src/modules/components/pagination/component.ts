2e1d82d4d2b17b508b0746896bb305298eb84750
Maks Zhitlov <maks.zhitlov@gmail.com>
2017-06-15 19:05:41 +0300

    add bindings, add dropdown, render buttons, add styles

--- src/modules/components/pagination/component.ts
+++ src/modules/components/pagination/component.ts
@@ -1,79 +1,127 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-
-const BASE_SHIFT = 0;
-const TITLE_SHIFT = 1;
+import {
+    ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output,
+    ViewEncapsulation
+} from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'adm-pagination',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None,
+    host: {
+        'class': 'adm-pagination'
+    },
+    styleUrls: ['./style.scss'],
     template: `
-        pagination component
+        <table>
+            <tr>
+                <td *ngIf="!showFullPagination && showDropdown">
+                    <adm-input-container [label]="titles.dropdownLabel">
+                        <adm-select single [options]="_range" [(ngModel)]="current" style="width: 100px;"
+                                    placeholder="Page">
+                        </adm-select>
+                    </adm-input-container>
+                </td>
+                <td>
+                    <adm-input-container [label]="titles.paginationLabel">
+                        <ul class="adm-pagination__list">
+                            <template [ngIf]="showPrevDots && !showFullPagination">
+                                <li>
+                                    <button (click)="setCurrent(1)" [disabled]="current == 1">{{titles.first}}</button>
+                                </li>
+                                <li>...</li>
+                            </template>
+                            <template ngFor let-num [ngForOf]="_range" let-i="index" [ngForTrackBy]="trackByFn">
+                                <li *ngIf="isShowNum(num.value) || showFullPagination">
+                                    <button [class.is__active]="num.value == current" (click)="setCurrent(num.value)">
+                                        {{num.label}}
+                                    </button>
+                                </li>
+                            </template>
+                            <template [ngIf]="showNextDots && !showFullPagination">
+                                <li>...</li>
+                                <li>
+                                    <button (click)="setCurrent(total)" [disabled]="current == total">{{titles.last}}
+                                    </button>
+                                </li>
+                            </template>
+                        </ul>
+                    </adm-input-container>
+                </td>
+            </tr>
+        </table>
+
     `
 })
 export class PaginationComponent implements OnInit {
+    _range: { label, value }[];
     @Input() current: number;
+    @Output() currentChange: EventEmitter<number> = new EventEmitter<number>();
+
     @Input() total: number;
-    @Input() visiblePages: number;
-    @Input() titles: { first, prev, prevSet, nextSet, next, last } = {
+    @Input() visiblePage: number;
+    @Input() titles: { first, prev, prevSet, nextSet, next, last, dropdownLabel, paginationLabel } = {
         first: 'First',
         prev: '\u00AB',
         prevSet: '...',
         nextSet: '...',
         next: '\u00BB',
-        last: 'Last'
+        last: 'Last',
+        dropdownLabel: 'Go to page',
+        paginationLabel: 'Pagination'
     };
+    @Input() showDropdown: boolean = true;
 
     @Output() onPageChanged: EventEmitter<number> = new EventEmitter<number>();
+
+    form: FormGroup = new FormGroup({
+        model: new FormControl('')
+    });
 
 
     constructor() {
     }
 
-
-    get calcBlocks(): { total, current, size } {
-        const total = Number(this.total);
-        const blockSize = Number(this.visiblePages);
-        const current = Number(this.current) + TITLE_SHIFT;
-        const blocks = Math.ceil(total / blockSize);
-        const currBlock = Math.ceil(current / blockSize) - TITLE_SHIFT;
-
-        return {
-            total: blocks,
-            current: currBlock,
-            size: blockSize
-        };
+    getRange(): { label, value }[] {
+        this._range = Array.from(Array(this.total).keys()).map(el => ({
+            label: (el + 1).toString(),
+            value: el + 1
+        }));
+        return this._range;
     }
 
-    get isPrevDisabled(): boolean {
-        return this.current <= BASE_SHIFT;
-    }
-
-    get isNextDisabled(): boolean {
-        return this.current >= (this.total - TITLE_SHIFT);
-    }
-
-    get isPrevMoreHidden(): boolean {
-        const blocks = this.calcBlocks;
-        return (blocks.total === TITLE_SHIFT) || (blocks.current === BASE_SHIFT);
-    }
-
-    get isNextMoreHidden(): boolean {
-        const blocks = this.calcBlocks;
-        return (blocks.total === TITLE_SHIFT) || (blocks.current === (blocks.total - TITLE_SHIFT));
-    }
-
-    get visibleRange() {
-        const blocks = this.calcBlocks;
-
-        console.log(blocks);
-
-        const start = blocks.current * blocks.size;
-        const delta = this.total - start;
-        const end = start + ((delta > blocks.size) ? blocks.size : delta);
-
-        return [start + TITLE_SHIFT, end + TITLE_SHIFT];
+    isShowNum(n: number) {
+        return (n == this.current) ||
+            (n == (this.current + 1)) ||
+            (n == (this.current + 2)) ||
+            (n == (this.current - 1)) ||
+            (n == (this.current - 2));
     }
 
     ngOnInit() {
-        console.log(this.visibleRange);
+        this.getRange();
+    }
+
+    trackByFn(item, index) {
+        return index;
+    }
+
+    get showPrevDots(): boolean {
+        return this.current > 3;
+    }
+
+    get showNextDots(): boolean {
+        return this.current < this.total - 2;
+    }
+
+    get showFullPagination(): boolean {
+        return this.total < 10;
+    }
+
+    setCurrent(n: number) {
+        this.current = n;
+        this.currentChange.emit(this.current);
+        this.onPageChanged.emit(this.current);
+        this.getRange();
     }
 }
--- src/modules/components/pagination/module.ts
+++ src/modules/components/pagination/module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PaginationComponent } from './component';
+import { CommonModule } from '@angular/common';
+import { SingleSelectModule } from '../single-select/module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputModule } from '../text-input/module';
 
 
 @NgModule({
-    imports: [],
+    imports: [CommonModule, SingleSelectModule, ReactiveFormsModule, FormsModule, InputModule],
     exports: [PaginationComponent],
     declarations: [PaginationComponent],
     providers: []
