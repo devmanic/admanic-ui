1374e82dece6d3cad400aa4b792f6a1bf25f8b33
Maks Zhitlov <maks.zhitlov@gmail.com>
2017-07-14 17:33:19 +0300

    add highlights, add ability select by index

--- src/modules/components/typeahead/component.ts
+++ src/modules/components/typeahead/component.ts
@@ -8,76 +8,81 @@ import { PopupService } from './popup.service';
 import { TypeaheadResultsComponent } from './results.component';
 
 enum Key {
-  Tab = 9,
-  Enter = 13,
-  Escape = 27,
-  ArrowUp = 38,
-  ArrowDown = 40
+    Tab = 9,
+    Enter = 13,
+    Escape = 27,
+    ArrowUp = 38,
+    ArrowDown = 40
 }
 
 const ADM_TYPEAHEAD_VALUE_ACCESSOR = {
-  provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => TypeaheadComponent),
-  multi: true
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => TypeaheadComponent),
+    multi: true
 };
 
 @Directive({
     selector: 'input[admTypeahead], textarea[admTypeahead]',
     providers: [ADM_TYPEAHEAD_VALUE_ACCESSOR],
-    host:{
-        '[class.is__open]':'isPopupOpen',
-        '[class.adm-typeahead]':'true'
+    host: {
+        '[class.is__open]': 'isPopupOpen',
+        '(keydown)': 'handleKeyDown($event)',
+        '(document:click)': 'dismissPopup()',
+        '(click)':'$event.stopPropagation();',
+        '[class.adm-typeahead]': 'true'
     }
 })
 
 export class TypeaheadComponent implements OnInit, OnDestroy {
-    _valueChanges:Observable<string>;
+    _valueChanges: Observable<string>;
     _userInput: string;
-    _subscription:Subscription;
-    _popupService:PopupService<TypeaheadResultsComponent>;
-    _windowRef:ComponentRef<TypeaheadResultsComponent>;
+    _subscription: Subscription;
+    _popupService: PopupService<TypeaheadResultsComponent>;
+    _windowRef: ComponentRef<TypeaheadResultsComponent>;
 
-    @Input() debounceTime:number = 300;
-    @Input() admTypeahead:(text:Observable<string>)=>Observable<any[]>;
+    @Input() debounceTime: number = 100;
+    @Input() admTypeahead: (text: Observable<string>) => Observable<any[]>;
     @Output() selectItem = new EventEmitter<any>();
 
     get isPopupOpen() {
-        return this._windowRef != null; 
+        return this._windowRef != null;
     }
 
-    private _onTouched = () => {};
-    private _onChange = (_: any) => {};
+    private toString(value) {
+        return (value !== undefined && value !== null) ? `${value}` : '';
+    }
+    private _onTouched = () => { };
+    private _onChange = (_: any) => { };
 
-    constructor(private _el: ElementRef, 
-                private _viewContainerRef: ViewContainerRef, 
-                private _renderer: Renderer,
-                private _injector: Injector, 
-                componentFactoryResolver: ComponentFactoryResolver,
-                ngZone: NgZone){ 
-        this._valueChanges = Observable.fromEvent(this._el.nativeElement, 'input', ($event:Event)=>(<HTMLInputElement>$event.target).value);
+    constructor(private _el: ElementRef,
+        private _viewContainerRef: ViewContainerRef,
+        private _renderer: Renderer,
+        private _injector: Injector,
+        componentFactoryResolver: ComponentFactoryResolver) {
+        this._valueChanges = Observable.fromEvent(this._el.nativeElement, 'input', ($event: Event) => (<HTMLInputElement>$event.target).value);
 
         this._popupService = new PopupService<TypeaheadResultsComponent>(
-            TypeaheadResultsComponent, 
-            _injector, 
+            TypeaheadResultsComponent,
+            _injector,
             _viewContainerRef,
-            _renderer, 
+            _renderer,
             componentFactoryResolver
         );
     }
 
     ngOnInit() {
         const inputValues$ = _do.call(this._valueChanges.debounceTime(this.debounceTime), value => {
-           this.writeValue(value);
+            this.writeValue(value);
         });
-        const results$ = letProto.call(inputValues$, this.admTypeahead);        
-        _do.call(this._valueChanges, (str:string)=>{
+        const results$ = letProto.call(inputValues$, this.admTypeahead);
+        _do.call(this._valueChanges, (str: string) => {
             _do.call(results$)
         });
         this._subscription = this._subscribeToUserInput(results$);
     }
 
-    _subscribeToUserInput(userInput:Observable<any[]>):Subscription{
-        return userInput.subscribe((results)=>{
+    _subscribeToUserInput(userInput: Observable<any[]>): Subscription {
+        return userInput.subscribe((results) => {
             if (!results || results.length === 0) {
                 this._closePopup();
             } else {
@@ -89,11 +94,6 @@ export class TypeaheadComponent implements OnInit, OnDestroy {
         })
     }
 
-    ngOnDestroy(){
-        this._subscription.unsubscribe();
-    }
-
-   
     _openPopup() {
         if (!this.isPopupOpen) {
             this._windowRef = this._popupService.open();
@@ -112,12 +112,59 @@ export class TypeaheadComponent implements OnInit, OnDestroy {
         this._closePopup();
     }
 
-    writeValue(value){
-        this._userInput = value;        
+    dismissPopup() {
+        if (this.isPopupOpen) {
+        this._closePopup();
+            this.writeValue(this._userInput);
+        }
+    }
+
+    writeValue(value) {
+        this._userInput = value;
+        if(!this._el.nativeElement.value && value){
+            this._el.nativeElement.value = value;
+        }
         this._onChange(value);
         this.selectItem.emit(value);
     }
 
+    handleKeyDown(event: KeyboardEvent) {
+        if (!this.isPopupOpen) {
+            return;
+        }
+
+        if (Key[this.toString(event.which)]) {
+            switch (event.which) {
+                case Key.ArrowDown:
+                    event.preventDefault();
+                    this._windowRef.instance.next();
+                    break;
+                case Key.ArrowUp:
+                    event.preventDefault();
+                    this._windowRef.instance.prev();
+                    break;
+                case Key.Enter:
+                case Key.Tab:
+                    const result = this._windowRef.instance.getActive();
+                    if (!!result) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                        this._selectResultClosePopup(result);
+                    }
+                    this._closePopup();
+                    break;
+                case Key.Escape:
+                    event.preventDefault();
+                    this.dismissPopup();
+                    break;
+            }
+        }
+    }
+
     registerOnChange(fn: (value: any) => any): void { this._onChange = fn; }
     registerOnTouched(fn: () => any): void { this._onTouched = fn; }
+
+    ngOnDestroy() {
+        this._subscription.unsubscribe();
+    }
 }
--- src/modules/components/typeahead/results.component.ts
+++ src/modules/components/typeahead/results.component.ts
@@ -1,27 +1,58 @@
 import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
-import { Pipe, PipeTransform, Output, EventEmitter } from '@angular/core';
+import { Pipe, PipeTransform, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 
 
 @Component({
     selector: 'adm-typeahead-results',
     encapsulation: ViewEncapsulation.None,
     styleUrls: ['../single-select/style.scss', './styles.scss'],
+    host:{
+        '(click)':'$event.stopPropagation()'
+    },
     template: `
         <div class="adm-select__options">
             <ul class="adm-select__options__list">
-                <li *ngFor="let item of results; trackBy: trackListByFn;" (click)="onItemClick(item);" [innerHtml]="item | higlight:query"></li>
+                <li 
+                    *ngFor="let item of results; let idx = index; trackBy: trackListByFn;" 
+                    [class.is__active]="idx === activeIdx"
+                    (click)="onItemClick(item);" 
+                    [innerHtml]="item | higlight:query"></li>
             </ul>
         </div>
     `
 })
 
-export class TypeaheadResultsComponent implements OnInit {
+export class TypeaheadResultsComponent {
     @Input() results:string[] = [];
     @Input() query:string;
     @Output('select') selectEvent = new EventEmitter();
-    constructor() { }
+    @Output('activeChange') activeChangeEvent = new EventEmitter();
 
-    ngOnInit() { }
+    focusFirst = true;
+    activeIdx = 0;
+
+    getActive() { return this.results[this.activeIdx]; }
+    markActive(activeIdx: number) {
+        this.activeIdx = activeIdx;
+    }
+
+    next() {
+        if (this.activeIdx === this.results.length - 1) {
+            this.activeIdx = this.focusFirst ? (this.activeIdx + 1) % this.results.length : -1;
+        } else {
+            this.activeIdx++;
+        }
+    }
+
+    prev() {
+        if (this.activeIdx < 0) {
+            this.activeIdx = this.results.length - 1;
+        } else if (this.activeIdx === 0) {
+            this.activeIdx = this.focusFirst ? this.results.length - 1 : -1;
+        } else {
+            this.activeIdx--;
+        }
+    }
 
     trackListByFn(index: number, item:string) {
         return item;
@@ -40,6 +71,6 @@ export class TypeaheadResultsComponent implements OnInit {
 
 export class HighlightPipe implements PipeTransform {
     transform(value:string, query:string): any {
-        return value.replace(new RegExp(query, 'gi'), str=> `<b>${str}</b>`);
+        return value.replace(new RegExp(query.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&'), 'gi'), str=> `<b>${str}</b>`);
     }
 }
