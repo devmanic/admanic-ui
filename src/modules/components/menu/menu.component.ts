0088d8496c551d8786840782312965dd676b34b3
Maks Zhitlov <maks.zhitlov@gmail.com>
2017-07-17 11:48:18 +0300

    create module template

---
+++ src/modules/components/menu/menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'adm-menu-component',
+    template: `
+        menu component
+    `
+})
+export class MenuComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
